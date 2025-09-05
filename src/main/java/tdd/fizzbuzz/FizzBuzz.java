931e1040938c10e2f5cbb0b23133f7f5f649c820
YUANJA3 <yuanja3@oocl.com>
2025-09-05 13:49:28 +0800

    refactor: refactor countOff 3,5,3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,22 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+
     public String countOff(int number) {
-        if(number%3==0&&number%5==0){
-            return "FizzBuzz";
+        boolean isCommonMultiple3And5 = number % 3 == 0 && number % 5 == 0;
+        boolean isMultiple3= number%3==0 ;
+        boolean isMultiple5= number%5==0;
+        if(isCommonMultiple3And5){
+            return FIZZ_BUZZ;
         }
-        else if (number%3==0){
-            return "Fizz";
+        else if (isMultiple3){
+            return FIZZ;
         }
-        else if(number%5==0){
-            return "Buzz";
+        else if(isMultiple5){
+            return BUZZ;
         }
         return String.valueOf(number);
     }
