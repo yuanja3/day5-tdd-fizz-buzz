108d17d9a8b10d5156a07ebb929855f50cb46ef5
YUANJA3 <yuanja3@oocl.com>
2025-09-05 11:51:27 +0800

    feat: implement countOff common multiple 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
-        if (number%3==0){
+        if(number%3==0&&number%5==0){
+            return "FizzBuzz";
+        }
+        else if (number%3==0){
             return "Fizz";
         }
         else if(number%5==0){
