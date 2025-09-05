ef9d72fe4b927cb99d0b236e17dc17c6bf1ec651
YUANJA3 <yuanja3@oocl.com>
2025-09-05 11:46:25 +0800

    feat: implement countOff multiple 3 test

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
+        if (number%3==0){
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
